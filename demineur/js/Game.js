45dcc29ebb72d1a2be1c28172935817ac227234b
= <paul.lecuisinier@gmail.com>
2023-09-13 16:34:49 +0200

    fin du jeu

--- demineur/js/Game.js
+++ demineur/js/Game.js
@@ -5,10 +5,10 @@ export default class Game {
 
     constructor(body, size, bombsNumber) {
         this.body = body;
+        this.remainingTile = size * size;
         this.genBombs(size, bombsNumber);
         this.fillGrid();
         this.playing = true;
-        this.remainingTile = size*size;
     }
 
     genBombs(size, bombsNumber) {
@@ -20,13 +20,17 @@ export default class Game {
             }
         }
         for (let i = 0; i < bombsNumber; i++) {
-            this.grid[Math.floor(Math.random() * size)][Math.floor(Math.random() * size)] = 'B';
+            const x = Math.floor(Math.random() * size);
+            const y = Math.floor(Math.random() * size);
+            if (this.grid[x][y] !== 'B') {
+                this.remainingTile--;
+            }
+            this.grid[x][y] = 'B';
         }
     }
 
     checkIfIsBomb(x, y, count) {
         if (this.grid[x][y] === 'B') {
-            this.remainingTile--;
             return count + 1;
         } else {
             return count;
@@ -72,6 +76,97 @@ export default class Game {
         }
     }
 
+    reveal() {
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid.length; j++) {
+                const selectedCase = document.getElementById(`${i}.${j}`);
+                if (selectedCase.classList.contains('flag')) {
+                    this.removeFlag(selectedCase);
+                }
+                if (this.grid[i][j] === 'B') {
+                    selectedCase.classList.add('bomb');
+                    const bombImage = document.createElement('img');
+                    bombImage.src = '../assets/bomb.png';
+                    bombImage.classList.add('bombImage');
+                    selectedCase.appendChild(bombImage);
+                } else {
+                    selectedCase.textContent = this.grid[i][j];
+                }
+            }
+        }
+    }
+
+    removeFlag(gridCase) {
+        gridCase.classList.remove('flag');
+        gridCase.innerHTML = '';
+    }
+
+    isChecked(checked, x, y) {
+        return checked.some(item => item.x === x && item.y === y);
+    }
+
+
+    revealAround(x, y, checked) {
+        checked.push({ x, y });
+
+        if (x - 1 >= 0) {
+            document.getElementById(`${x - 1}.${y}`).textContent = this.grid[x - 1][y];
+            this.remainingTile--;
+            if (this.grid[x - 1][y] === 0 && !this.isChecked(checked, x - 1, y)) {
+                this.revealAround(x - 1, y, checked);
+            }
+        }
+        if (y - 1 >= 0) {
+            document.getElementById(`${x}.${y - 1}`).textContent = this.grid[x][y - 1];
+            this.remainingTile--;
+            if (this.grid[x][y - 1] === 0 && !this.isChecked(checked, x, y - 1)) {
+                this.revealAround(x, y - 1, checked);
+            }
+        }
+        if (x + 1 < this.grid.length) {
+            document.getElementById(`${x + 1}.${y}`).textContent = this.grid[x + 1][y];
+            this.remainingTile--;
+            if (this.grid[x + 1][y] === 0 && !this.isChecked(checked, x + 1, y)) {
+                this.revealAround(x + 1, y, checked);
+            }
+        }
+        if (y + 1 < this.grid.length) {
+            document.getElementById(`${x}.${y + 1}`).textContent = this.grid[x][y + 1];
+            this.remainingTile--;
+            if (this.grid[x][y + 1] === 0 && !this.isChecked(checked, x, y + 1)) {
+                this.revealAround(x, y + 1, checked);
+            }
+        }
+        if (x - 1 >= 0 && y - 1 >= 0) {
+            document.getElementById(`${x - 1}.${y - 1}`).textContent = this.grid[x - 1][y - 1];
+            this.remainingTile--;
+            if (this.grid[x - 1][y - 1] === 0 && !this.isChecked(checked, x - 1, y - 1)) {
+                this.revealAround(x - 1, y - 1, checked);
+            }
+        }
+        if (x - 1 >= 0 && y + 1 < this.grid.length) {
+            document.getElementById(`${x - 1}.${y + 1}`).textContent = this.grid[x - 1][y + 1];
+            this.remainingTile--;
+            if (this.grid[x - 1][y + 1] === 0 && !this.isChecked(checked, x - 1, y + 1)) {
+                this.revealAround(x - 1, y + 1, checked);
+            }
+        }
+        if (x + 1 < this.grid.length && y - 1 >= 0) {
+            document.getElementById(`${x + 1}.${y - 1}`).textContent = this.grid[x + 1][y - 1];
+            this.remainingTile--;
+            if (this.grid[x + 1][y - 1] === 0 && !this.isChecked(checked, x + 1, y - 1)) {
+                this.revealAround(x + 1, y - 1, checked);
+            }
+        }
+        if (x + 1 < this.grid.length && y + 1 < this.grid.length) {
+            document.getElementById(`${x + 1}.${y + 1}`).textContent = this.grid[x + 1][y + 1];
+            this.remainingTile--;
+            if (this.grid[x + 1][y + 1] === 0 && !this.isChecked(checked, x + 1, y + 1)) {
+                this.revealAround(x + 1, y + 1, checked);
+            }
+        }
+    }
+
     render() {
         let rawNumber = this.grid.length;
         let columnNumber = this.grid[0].length;
@@ -92,26 +187,44 @@ export default class Game {
                 newCase.setAttribute('id', `${raw.length - columnNumber}.${i}`);
                 element.appendChild(newCase);
                 newCase.addEventListener('click', () => {
-                    if(this.playing){
-                    const x = Number(newCase.getAttribute('id').split('.')[0]);
-                    const y = Number(newCase.getAttribute('id').split('.')[1]);
-                    if (this.grid[x][y] === 'B') {
-                        newCase.classList.add('bomb');
-                        this.playing = false;
-                        document.getElementById('message').textContent = 'Perdu !';
-                        let bombImage = document.createElement('img');
-                        bombImage.src = '../assets/bomb.png';
-                        bombImage.classList.add('bombImage');
-                        newCase.appendChild(bombImage);
-                    } else {
-                        newCase.textContent = this.grid[x][y];
-                        this.remainingTile--;
-                        if(!this.remainingTile){
+                    if (this.playing && !newCase.classList.contains('flag') && !newCase.classList.contains('revealed')) {
+                        newCase.classList.add('revealed');
+                        const x = Number(newCase.getAttribute('id').split('.')[0]);
+                        const y = Number(newCase.getAttribute('id').split('.')[1]);
+                        if (this.grid[x][y] === 'B') {
+                            newCase.classList.add('bomb');
                             this.playing = false;
-                            document.getElementById('message').textContent = 'Gagné !'
+                            document.getElementById('message').textContent = 'Perdu !';
+                            this.reveal();
+                        } else {
+                            newCase.textContent = this.grid[x][y];
+                            this.remainingTile--;
+                            if (newCase.textContent === '0') {
+                                this.revealAround(x, y, []);
+                            }
+                            if (!this.remainingTile) {
+                                this.playing = false;
+                                document.getElementById('message').textContent = 'Gagné !'
+                                document.getElementById('resultEffect').classList.add('pyro');
+                                this.reveal();
+                            }
                         }
                     }
-                }
+                });
+                newCase.addEventListener("contextmenu", (event) => {
+                    event.preventDefault();
+                    if (this.playing && !newCase.classList.contains('revealed')) {
+                        if (!newCase.classList.contains('flag')) {
+                            newCase.classList.add('flag');
+                            const flagImage = document.createElement('img');
+                            flagImage.src = '../assets/flag.png';
+                            flagImage.classList.add('flagImage');
+                            newCase.appendChild(flagImage);
+                        } else {
+                            this.removeFlag(newCase);
+                        }
+                    }
+                    event.preventDefault();
                 });
                 i = i + 1;
             });
@@ -119,5 +232,5 @@ export default class Game {
         }
     }
 
-    
+
 }
--- demineur/js/main.js
+++ demineur/js/main.js
@@ -1,7 +1,58 @@
 import * as Game from './Game.js';
 
-let body = document.querySelector('.direction-column');
+const difficulties = {
+    easy: {gridSize: 5, bombsNumber: 6},
+    normal: {gridSize: 8, bombsNumber: 15},
+    hard: {gridSize: 15, bombsNumber: 50},
+    extreme: {gridSize: 25, bombsNumber: 200},
+};
 
-const game = new Game.default(body, 25, 200);
+const body = document.querySelector('.direction-column');
 
-game.render();
+const gridSize = document.getElementById('gridSize');
+const bombsNumber = document.getElementById('bombsNumber');
+const difficulty = document.getElementById('difficulty');
+
+document.getElementById('game').hidden = true;
+
+const setValues = (gridSizeValue = gridSize.value, custom = true, difficulty = 'custom') => {
+    gridSize.value = gridSizeValue;
+    if (custom) {
+        bombsNumber.value = Math.floor(bombsNumber.max / 2);
+        bombsNumber.max = Math.floor(Math.pow(gridSize.value, 2) * 0.35);
+    } else {
+        bombsNumber.max = difficulties[difficulty].bombsNumber * 2;
+        bombsNumber.value = Math.floor(difficulties[difficulty].bombsNumber);
+    }
+    document.getElementById('gridSizeLabel').textContent = gridSize.value.toString();
+    document.getElementById('bombsNumberLabel').textContent = bombsNumber.value.toString();
+};
+
+const initSelect = () => {
+    difficulty.value = 'normal';
+    gridSize.value = difficulties.normal.gridSize;
+    bombsNumber.value = difficulties.normal.bombsNumber;
+    setValues(gridSize.value, false, difficulty.value);
+};
+
+initSelect();
+
+difficulty.addEventListener('change', (e) => {
+    setValues(difficulties[difficulty.value].gridSize, false, difficulty.value);
+});
+
+document.getElementById('play').addEventListener('click', () => {
+    document.getElementById('home').hidden = true;
+    document.getElementById('game').hidden = false;
+    const game = new Game.default(body, gridSize.value, bombsNumber.value);
+    game.render();
+});
+gridSize.addEventListener('input', () => {
+    setValues();
+    difficulty.value = 'custom';
+});
+
+bombsNumber.addEventListener('input', () => {
+    document.getElementById('bombsNumberLabel').textContent = bombsNumber.value.toString();
+    difficulty.value = 'custom';
+});
